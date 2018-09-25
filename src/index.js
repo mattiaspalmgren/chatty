11b6fdf9c2e0619a5e3edf85a29137cd3ecb4204
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2018-09-25 15:57:03 +0200

    Render dummy user

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App/App.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const user = {
+    name: "Alice",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+    " Donec ligula arcu, mollis at consectetur sit amet, porta gravida" +
+    " ligula. Aenean pellentesque lobortis urna."
+};
+
+ReactDOM.render(<App user={user} />, document.getElementById("root"));
