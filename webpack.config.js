038bce1af166c4e9316f220446808f51e6f6b82f
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2018-09-27 13:47:29 +0200

    Include babel settings in webpack config and create loader objects

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,36 @@
 const path = require("path");
 const webpack = require("webpack");
 
+const jsLoader = {
+    test: /\.(js|jsx)$/,
+    exclude: /(node_modules|bower_components)/,
+    loader: "babel-loader",
+    options: { presets: ["@babel/preset-env", "@babel/preset-react"] },
+    include: __dirname + '/src',
+};
+
+const cssLoader = {
+    test: /\.css$/,
+    use: [
+            { loader: "style-loader" },
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+                modules: true,
+                localIdentName: "[local]___[hash:base64:5]"
+              }
+            }
+          ]
+};
+
 module.exports = {
   entry: "./src/index.js",
   mode: "development",
   module: {
     rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] },
-        include: __dirname + '/src',
-      },
-      {
-        test: /\.css$/,
-        use: [
-                { loader: "style-loader" },
-                {
-                  loader: "css-loader",
-                  options: {
-                    sourceMap: true,
-                    modules: true,
-                    localIdentName: "[local]___[hash:base64:5]"
-                  }
-                }
-        ]
-      }
+      jsLoader,
+      cssLoader
     ]
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
