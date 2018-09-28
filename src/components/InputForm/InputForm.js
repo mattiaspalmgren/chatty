f57ab85ebca9417aa2550950bc3ce09e3b5893c1
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2018-09-28 13:50:01 +0200

    Add wrapper and align row items correctly

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -6,13 +6,11 @@ import style from './InputForm.css';
 class InputForm extends Component {
     render() {
         return (
-            <div>
-                <form className={style.form}>
-                    <input className={style.field} type="text" name="name" placeholder="name"></input>
-                    <input className={style.field} type="text" name="email" placeholder="email"></input>
-                    <Button text="Submit"></Button>
-                </form>
-            </div>
+            <form className={style.form}>
+                <input className={style.field} type="text" name="name" placeholder="name"></input>
+                <input className={style.field} type="text" name="email" placeholder="email"></input>
+                <Button text="Submit"></Button>
+            </form>
         );
     }
 }
