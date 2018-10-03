73230143ad773ecbeff3018d92db53960e0d20cd
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2018-10-03 08:43:07 +0200

    Correct submitting from button to form

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,20 +2,13 @@ import React, { Component } from 'react';
 
 import style from "./Button.css";
 
-class Button extends Component {
-    constructor(props) {
-        super(props);
-        const { text, onSubmit } = this.props;
-    }
-    render() {
-        const { text, onSubmit } = this.props;
-        const styleClass = this.props.error ? style.error : style.Button;
-        return (
-            <button className={styleClass}>
-            {text}
-            </button>
-        );
-    }
+const Button = ({ text, error }) => {
+    const styleClass = error ? style.error : style.Button;
+    return (
+        <button className={styleClass}>
+        {text}
+        </button>
+    );
 }
 
 export default Button;
--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -5,14 +5,14 @@ import type from '../../types/index.js';
 import style from './InputForm.css';
 
 const InputForm = ({ inputForm }) => (
-    <form className={style.form}>
+    <form className={style.form} onSubmit={inputForm.onSubmit}>
         <h2 className={style.title}>{inputForm.title}</h2>
         {
             inputForm.fields.map(field => (
                 <input key={field} className={style.field} type="text" name={field} placeholder={field}/>
             ))
         }
-        <Button text="Submit" onSubmit={inputForm.onSubmit}></Button>
+        <Button text="Submit"></Button>
     </form>
 );
 
