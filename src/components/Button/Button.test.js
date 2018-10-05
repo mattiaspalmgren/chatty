7bef9438eabdfc4a1dcb5bb58d4b3562e155663c
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2018-10-05 11:58:06 +0200

    Remove tests

--- src/components/Button/Button.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { shallow } from 'enzyme';
-import Button from "./Button.js";
-
-test("Button changes class when hovered", () => {
-    const component = shallow(<Button/>);
-    expect(component).toMatchSnapshot();
-})
